0f9b972cddbd682f851ecf7171c07380bf39b66b
acalle11 <34049946+acalle11@users.noreply.github.com>
2018-08-28 14:19:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,31 @@ var Article = require("./models/Article.js");
 var htmlRouter = require("./controllers/html-routes.js");
 var articleRouter = require("./controllers/article-routes.js");
 
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+var url = 'mongodb://username:password@ds01316.mlab.com:1316/food';      
+//(Focus on This Variable)
+
+// Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+
 // Initialize Express
 var port = process.env.PORT || 3001;
 var app = express();
